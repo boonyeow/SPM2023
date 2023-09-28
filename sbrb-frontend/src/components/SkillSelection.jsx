411e4f3f6cb8e87ac8faabd008459bc0f2eec694
rrachea <rrachelsng@gmail.com>
2023-09-29 00:29:29 +0800

    feat(ui): Create Skill Selection Component
    add and remove skills when creating role

---
+++ sbrb-frontend/src/components/SkillSelection.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import {
+  GridItem,
+  Select,
+  Badge,
+  Text,
+} from '@chakra-ui/react';
+
+function SkillSelection() {
+  const [selectedSkills, setSelectedSkills] = useState([]);
+
+  const handleSkillSelect = (e) => {
+    const selectedSkill = e.target.value;
+    if (!selectedSkills.includes(selectedSkill)) {
+      setSelectedSkills([...selectedSkills, selectedSkill]);
+    }
+  };
+
+  const handleSkillRemove = (skillToRemove) => {
+    const updatedSkills = selectedSkills.filter((skill) => skill !== skillToRemove);
+    setSelectedSkills(updatedSkills);
+  };
+
+  const skills = [ 
+  'Communication Skills',
+  'Teamwork',
+  'Problem Solving',
+  'Leadership',
+  'Time Management',
+  'Adaptability',
+  'Creativity',
+  'Critical Thinking',
+  'Project Management',
+  'Data Analysis',];
+
+  return (
+    <GridItem colSpan={2} bg='papayawhip'>
+      <Text mb="8px">Skills Required:</Text>
+
+      <Select
+        placeholder='Select option'
+        onChange={handleSkillSelect}
+      >
+        {skills.map((skill, index) => (
+          <option key={index} value={skill}>
+            {skill}
+          </option>
+        ))}
+      </Select>
+
+      {selectedSkills.length > 0 && (
+        <div>
+          <Text mt="8px">Selected Skills:</Text>  <Text fontSize={'12px'}>Click the badge to remove skill</Text>
+          {selectedSkills.map((skill, index) => (
+            <Badge
+              key={index}
+              colorScheme="blue"
+              variant="outline"
+              cursor="pointer"
+              ml="2"
+              mt="2"
+              onClick={() => handleSkillRemove(skill)}
+            >
+              {skill}
+            </Badge>
+          ))}
+        </div>
+      )}
+    </GridItem>
+  );
+}
+
+export default SkillSelection;
