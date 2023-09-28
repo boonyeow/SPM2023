db88ac7c2dc7b13b105d7688ed6948c6af72caba
rrachea <rrachelsng@gmail.com>
2023-09-28 23:12:28 +0800

    feat(ui): update feature component to display jobs
    added badges to display skills

--- sbrb-frontend/src/components/Feature.jsx
+++ sbrb-frontend/src/components/Feature.jsx
@@ -1,25 +1,21 @@
-import { Box, Heading, Text } from '@chakra-ui/react';
-
-function Feature({ title, desc,manager, location, startDate,  deadline, percentageMatch, ...rest }) {
+import { Box, Heading, Text, Badge } from '@chakra-ui/react';
+function Feature({ title, desc, manager, location,  deadline, skills, ...rest }) {
     return (
       <Box p={5} shadow='md' borderWidth='1px' {...rest}>
         <Heading fontSize='xl'>{title}</Heading>
         <Text mt={4}>{desc}</Text>
         <Text>Country: {location}</Text>
         <Text>Reporting Manager: {manager}</Text>
-        <Text>Application starting date: {startDate}</Text>
+        <Text>Skills Required:</Text>
+        {skills.map((skill, index)=>(
+          <Badge ml='1' fontSize='0.8em' colorScheme='green'>
+          {skill}
+        </Badge>
+        ))}
         <Text>Application deadline: {deadline}</Text>
-        <Text>Skills Match: {percentageMatch}</Text>
       </Box>
     )
 }
 
-// proj cla say got starting date
-// ○     Role Title
-// ○     Location of Job
-// ○     Short Role Description
-// ○     Required skills for the role.
-// ○     Application deadline for the role.
-// ○     Percentage of my skills matching the role's requirements
 
 export default Feature;
