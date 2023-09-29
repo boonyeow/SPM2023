05b3d46c2fb90e58cc1a10669064073a68abda10
rrachea <81131034+rrachea@users.noreply.github.com>
2023-09-29 14:36:18 +0800

    feat(ui): SPM-8 Browse Open Roles for Staff (#5)
    * update NavBar
    
    created NavBar and changed the field JobListings to JobApplicants so that Home will be used to display all JobListings. ensured that it is mobile responsive.
    
    * Create Feature Component to display each JobRoleListing
    
    to be used for all job role listings
    
    * feat(ui): update feature component to display jobs
    
    added badges to display skills
    
    * feat(ui): complete user story SPM-8
    
    view job listings with dummy data
    
    * chore(lint): fix linting issues
    
    * chore(component): Rename 'Feature' component to 'RoleListingCard'
    
    ---------
    
    Co-authored-by: Alvin <lwc.alvin.al@gmail.com>

---
+++ sbrb-frontend/src/components/RoleListingCard.jsx
@@ -0,0 +1,29 @@
+import { Badge, Box, Heading, Text } from "@chakra-ui/react";
+
+function RoleListingCard({
+  title,
+  desc,
+  manager,
+  location,
+  deadline,
+  skills,
+  ...rest
+}) {
+  return (
+    <Box p={5} shadow="md" borderWidth="1px" {...rest}>
+      <Heading fontSize="xl">{title}</Heading>
+      <Text mt={4}>{desc}</Text>
+      <Text>Country: {location}</Text>
+      <Text>Reporting Manager: {manager}</Text>
+      <Text>Skills Required:</Text>
+      {skills.map((skill) => (
+        <Badge ml="1" fontSize="0.8em" colorScheme="green" key={skill}>
+          {skill}
+        </Badge>
+      ))}
+      <Text>Application deadline: {deadline}</Text>
+    </Box>
+  );
+}
+
+export default RoleListingCard;
