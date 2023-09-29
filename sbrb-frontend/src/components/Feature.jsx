30645f8299d6e00cba7a6f82a79af17b78ce4f22
Alvin <lwc.alvin.al@gmail.com>
2023-09-29 13:48:17 +0800

    chore(lint): fix linting issues

--- sbrb-frontend/src/components/Feature.jsx
+++ sbrb-frontend/src/components/Feature.jsx
@@ -1,21 +1,29 @@
-import { Box, Heading, Text, Badge } from '@chakra-ui/react';
-function Feature({ title, desc, manager, location,  deadline, skills, ...rest }) {
-    return (
-      <Box p={5} shadow='md' borderWidth='1px' {...rest}>
-        <Heading fontSize='xl'>{title}</Heading>
-        <Text mt={4}>{desc}</Text>
-        <Text>Country: {location}</Text>
-        <Text>Reporting Manager: {manager}</Text>
-        <Text>Skills Required:</Text>
-        {skills.map((skill, index)=>(
-          <Badge ml='1' fontSize='0.8em' colorScheme='green'>
+import { Badge, Box, Heading, Text } from "@chakra-ui/react";
+
+function Feature({
+  title,
+  desc,
+  manager,
+  location,
+  deadline,
+  skills,
+  ...rest
+}) {
+  return (
+    <Box p={5} shadow="md" borderWidth="1px" {...rest}>
+      <Heading fontSize="xl">{title}</Heading>
+      <Text mt={4}>{desc}</Text>
+      <Text>Country: {location}</Text>
+      <Text>Reporting Manager: {manager}</Text>
+      <Text>Skills Required:</Text>
+      {skills.map((skill) => (
+        <Badge ml="1" fontSize="0.8em" colorScheme="green" key={skill}>
           {skill}
         </Badge>
-        ))}
-        <Text>Application deadline: {deadline}</Text>
-      </Box>
-    )
+      ))}
+      <Text>Application deadline: {deadline}</Text>
+    </Box>
+  );
 }
-
 
 export default Feature;
