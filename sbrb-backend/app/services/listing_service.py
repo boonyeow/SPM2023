3050987e7f591ebac388da25b5aa2b55d1f13e9b
BY <tanboonyeow@outlook.com>
2023-10-08 21:33:56 +0800

    feat: Fetch all open role listing from database (ref: SPM-36) (#18)
    * Added query param to filter active listings
    * Minor fix for black auto format
    
    ---------
    
    Co-authored-by: Github Actions <actions.github.com>

--- sbrb-backend/app/services/listing_service.py
+++ sbrb-backend/app/services/listing_service.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from sqlalchemy.orm import Session
 
 from app.models import Listing, Staff
@@ -9,7 +11,27 @@ class ListingService:
         self.db = db
 
     def get_all_listings_with_skills(self):
-        listings = self.db.query(Listing).all()
+        return self.get_listings_with_skills(active=None)
+
+    def get_active_listings_with_skills(self):
+        return self.get_listings_with_skills(active=True)
+
+    def get_inactive_listings_with_skills(self):
+        return self.get_listings_with_skills(active=False)
+
+    def get_listings_with_skills(self, active=None):
+        listings_query = self.db.query(Listing)
+        if active is None:
+            pass
+        elif active:
+            listings_query = listings_query.filter(
+                Listing.expiry_date >= datetime.utcnow()
+            )
+        else:
+            listings_query = listings_query.filter(
+                Listing.expiry_date < datetime.utcnow()
+            )
+        listings = listings_query.all()
         result = []
 
         for listing in listings:
