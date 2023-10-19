412cd82afe0a1e42b676c0e3206c2e9cb66b5cb3
BY <tanboonyeow@outlook.com>
2023-10-19 14:51:30 +0800

    feat: Create role listing (ref: SPM-62) (#24)
    * Added create listing
    * Added country, department model
    * Updated yaml to load env variables
    * Updated working directory for backend tests
    * Updated form with formik
    * Read CSV with encoding
    * Fixed linting issues
    ---------
    
    Co-authored-by: Alvin Ling <66348533+AlvinLingg@users.noreply.github.com>
    Co-authored-by: Alvin <lwc.alvin.al@gmail.com>

--- sbrb-backend/app/services/listing_service.py
+++ sbrb-backend/app/services/listing_service.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from sqlalchemy.orm import Session
 
 from app.models import Listing
-from app.schemas.listing_schema import ListingWithSkills
+from app.schemas.listing_schema import ListingCreate, ListingWithSkills
 
 
 def get_staff_name(first_name, last_name):
@@ -13,12 +13,29 @@ def get_staff_name(first_name, last_name):
 class ListingService:
     def __init__(self, db: Session):
         self.db = db
-        
+
     def check_if_listing_is_active(self, listing_id):
         listing = self.db.get(Listing, listing_id)
         if listing.expiry_date >= datetime.utcnow():
             return True
         return False
+
+    def create_listing(self, body: ListingCreate):
+        new_listing = Listing(
+            role_name=body.role_name,
+            listing_title=body.listing_title,
+            listing_desc=body.listing_desc,
+            department_name=body.dept,
+            country_name=body.country,
+            reporting_manager_id=body.reporting_manager_id,
+            created_by_id=body.created_by_id,
+            expiry_date=body.expiry_date,
+        )
+
+        self.db.add(new_listing)
+        self.db.commit()
+        self.db.refresh(new_listing)
+        return new_listing
 
     def get_listings_with_skills(self, active=None):
         listings_query = self.db.query(Listing)
--- sbrb-backend/main.py
+++ sbrb-backend/main.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import APIRouter, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.database import init_engine
 from app.routers import application_route, listing_route
@@ -15,6 +16,12 @@ def get_root():
 init_engine(is_test=True)
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # Include your API routers
 app.include_router(api_router)
 app.include_router(application_route.router)
