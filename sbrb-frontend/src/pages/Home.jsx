3cfd03a8a8d5ccc5e6ca582d58ddcadecbd03c4e
rrachea <81131034+rrachea@users.noreply.github.com>
2023-10-24 12:23:29 +0800

    feat(ui): Filter open roles by skills SPM-17 (#35)
    * match if listings contain filtered skills
    
    * format code
    
    * use autocomplete input
    
    * remove comment
    
    * remove search term

--- sbrb-frontend/src/pages/Home.jsx
+++ sbrb-frontend/src/pages/Home.jsx
@@ -25,12 +25,19 @@ const Home = () => {
     if (!isAllUnchecked) {
       if (checkedValues.departments.length > 0) {
         filteredListings = filteredListings.filter((listing) =>
-          checkedValues.departments.includes(listing.dept)
+          checkedValues.departments.includes(listing.department_name)
         );
       }
+
+      if (checkedValues.skills.length > 0) {
+        filteredListings = filteredListings.filter((listing) =>
+          checkedValues.skills.every((skill) => listing.skills.includes(skill))
+        );
+      }
+
       if (checkedValues.countries.length > 0) {
         filteredListings = filteredListings.filter((listing) =>
-          checkedValues.countries.includes(listing.country)
+          checkedValues.countries.includes(listing.country_name)
         );
       }
     } else {
@@ -64,7 +71,6 @@ const Home = () => {
             {/* display all jobs */}
             <Box w="90%" p={20}>
               <Grid
-                h="100vh"
                 templateRows="repeat(2, 1fr)"
                 templateColumns="repeat(5, 1fr)"
                 gap={4}>
