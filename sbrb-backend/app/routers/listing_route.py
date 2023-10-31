e05e034e9490049aa6df4cc91b4c286cad4ee494
BY <9499796+boonyeow@users.noreply.github.com>
2023-10-31 16:24:36 +0800

    added endpoint for edit_listing + helper service for checks

--- sbrb-backend/app/routers/listing_route.py
+++ sbrb-backend/app/routers/listing_route.py
@@ -1,13 +1,13 @@
 from typing import List
 
-from fastapi import APIRouter, Depends, Query
-from sqlalchemy.orm import Session
-
 from app.database import get_db
 from app.schemas.application_schema import ApplicationWithStaffSkills
 from app.schemas.listing_schema import Listing, ListingCreate, ListingWithSkills
 from app.services.application_service import ApplicationService
+from app.services.helper_service import HelperService
 from app.services.listing_service import ListingService
+from fastapi import APIRouter, Depends, HTTPException, Query
+from sqlalchemy.orm import Session
 
 router = APIRouter()
 
@@ -57,3 +57,49 @@ def get_applicants_for_listing(id: int, db: Session = Depends(get_db)):
 def create_listing(body: ListingCreate, db: Session = Depends(get_db)):
     listing_service = ListingService(db)
     return listing_service.create_listing(body)
+
+
+@router.put("/listings/{id}", status_code=200, response_model=Listing)
+def edit_listing(id: int, body: ListingCreate, db: Session = Depends(get_db)):
+    if body.listing_title == "":
+        raise HTTPException(status_code=403, detail="Listing title cannot be empty")
+
+    if body.listing_desc == "":
+        raise HTTPException(
+            status_code=403, detail="Listing description cannot be empty"
+        )
+
+    helper_service = HelperService(db)
+    country_name_exist = helper_service.check_if_country_exists(body.country_name)
+    if not country_name_exist:
+        raise HTTPException(status_code=403, detail="Country does not exist")
+
+    department_name_exist = helper_service.check_if_department_exists(
+        body.department_name
+    )
+    if not department_name_exist:
+        raise HTTPException(status_code=403, detail="Department does not exist")
+
+    role_name_exist = helper_service.check_if_role_exists(body.role_name)
+    if not role_name_exist:
+        raise HTTPException(status_code=403, detail="Role does not exist")
+
+    created_by_exists = helper_service.check_if_staff_exists(body.created_by_id)
+    if not created_by_exists:
+        raise HTTPException(status_code=403, detail="Created By does not exist")
+
+    reporting_manager_exists = helper_service.check_if_staff_exists(
+        body.reporting_manager_id
+    )
+    if not reporting_manager_exists:
+        raise HTTPException(status_code=403, detail="Reporting Manager does not exist")
+
+    listing_service = ListingService(db)
+    listing_exist = listing_service.check_if_listing_exists(id)
+    if not listing_exist:
+        raise HTTPException(status_code=403, detail="Listing does not exist")
+
+    listing_active = listing_service.check_if_listing_is_active(id)
+    if not listing_active:
+        raise HTTPException(status_code=403, detail="Listing is not active")
+    return listing_service.update_listing(id, body)
