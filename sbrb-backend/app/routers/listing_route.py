b7355fd0f07f897d68682bc5e3201d36d7a8004d
BY <9499796+boonyeow@users.noreply.github.com>
2023-11-01 13:23:25 +0800

    added httpexceptions for create listing

--- sbrb-backend/app/routers/listing_route.py
+++ sbrb-backend/app/routers/listing_route.py
@@ -55,6 +55,39 @@ def get_applicants_for_listing(id: int, db: Session = Depends(get_db)):
 
 @router.post("/listing/create", status_code=200, response_model=Listing)
 def create_listing(body: ListingCreate, db: Session = Depends(get_db)):
+    if body.listing_title == "":
+        raise HTTPException(status_code=403, detail="Listing title cannot be empty")
+
+    if body.listing_desc == "":
+        raise HTTPException(
+            status_code=403, detail="Listing description cannot be empty"
+        )
+
+    helper_service = HelperService(db)
+    country_name_exist = helper_service.check_if_country_exists(body.country_name)
+    if not country_name_exist:
+        raise HTTPException(status_code=403, detail="Country does not exist")
+
+    department_name_exist = helper_service.check_if_department_exists(
+        body.department_name
+    )
+    if not department_name_exist:
+        raise HTTPException(status_code=403, detail="Department does not exist")
+
+    role_name_exist = helper_service.check_if_role_exists(body.role_name)
+    if not role_name_exist:
+        raise HTTPException(status_code=403, detail="Role does not exist")
+
+    created_by_exists = helper_service.check_if_staff_exists(body.created_by_id)
+    if not created_by_exists:
+        raise HTTPException(status_code=403, detail="Created By does not exist")
+
+    reporting_manager_exists = helper_service.check_if_staff_exists(
+        body.reporting_manager_id
+    )
+    if not reporting_manager_exists:
+        raise HTTPException(status_code=403, detail="Reporting Manager does not exist")
+
     listing_service = ListingService(db)
     return listing_service.create_listing(body)
 
